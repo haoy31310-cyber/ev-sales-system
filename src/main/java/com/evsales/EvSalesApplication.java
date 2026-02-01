f503ac1c0f84e002eb52b618ce530ca3559ca45d
haoy31310-cyber <haoy31310@gmail.com>
2026-02-01 13:19:17 +0800

    Update EvSalesApplication.java

--- src/main/java/com/evsales/EvSalesApplication.java
+++ src/main/java/com/evsales/EvSalesApplication.java
@@ -1,1 +1,12 @@
 
+package com.evsales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvSalesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EvSalesApplication.class, args);
+    }
+}
